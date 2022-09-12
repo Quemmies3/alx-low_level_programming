4c2f87bd1feee82c21a6bc6c6e42351f2f44c06d
Olu <Quemestree@gmail.com>
2022-09-11 18:41:53 -0700

    Program that prints Programming is like building a multilingual puzzle, followed by a new line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - This is the body of the code
+ *
+ * Return: Must return an integer value always zero
+ */
+
+int main(void)
+{
+	char a[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(a);
+
+	return (0);
+}

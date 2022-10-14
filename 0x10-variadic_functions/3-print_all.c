46c51d3a3e6c78a6badcc64ff4852efed7c8162d
Olukemi Odedeyi <80988691+Quemmies3@users.noreply.github.com>
2022-10-14 01:48:13 +0100

    function that prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -13,7 +13,7 @@ void print_all(const char * const format, ...)
 {
 	va_list valist;
 
-	unsigned int i = 0, j, c = 0;
+	int i = 0, j, c = 0;
 
 	char *str;
 
@@ -34,7 +34,7 @@ void print_all(const char * const format, ...)
 		switch (format[i])
 		{
 		case 'c':
-			printf("%c", va_arg(valist, int)), c = 1;
+			printf("%c", va_arg(valist, char)), c = 1;
 			break;
 		case 'i':
 			printf("%i", va_arg(valist, int)), c = 1;

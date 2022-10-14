1883620afca7c1d832c42bce8c74848b8ea85c51
Olukemi Odedeyi <80988691+Quemmies3@users.noreply.github.com>
2022-10-14 01:05:36 +0100

    function that prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,5 +1,74 @@
 #include "variadic_functions.h"
 
+void print_char(va_list valist);
+void print_int(va_list valist);
+void print_float(va_list valist);
+void print_string(va_list valist);
+void print_all(const char * const format, ...);
+
+/**
+ * print_char - Prints a char.
+ * @valist: A list of arguments pointing to
+ *       the character to be printed.
+ */
+
+void print_char(va_list valist)
+{
+	char letter;
+
+	letter = va_arg(valist, int);
+	printf("%c", letter);
+}
+
+/**
+ * print_int - Prints an int.
+ * @valist: A list of arguments pointing to
+ *       the integer to be printed.
+ */
+
+void print_int(va_list valist)
+{
+	int num;
+
+	num = va_arg(valist, int);
+	printf("%d", num);
+}
+
+/**
+ * print_float - Prints a float.
+ * @valist: A list of arguments pointing to
+ *       the float to be printed.
+ */
+
+void print_float(va_list valist)
+{
+	float num;
+
+	num = va_arg(valist, double);
+	printf("%f", num);
+}
+
+/**
+ * print_string - Prints a string.
+ * @valist: A list of arguments pointing to
+ *       the string to be printed.
+ */
+
+void print_string(va_list valist)
+{
+	char *str;
+
+	str = va_arg(valist, char *);
+
+	if (str == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+
+	printf("%s", str);
+}
+
 /**
  * print_all - prints anything.
  * @format: a list of types of arguments passed to the function.

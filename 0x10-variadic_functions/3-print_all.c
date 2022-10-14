f96baf5318255bd521ec32c83f4fac84478e6414
Olukemi Odedeyi <80988691+Quemmies3@users.noreply.github.com>
2022-10-14 13:03:34 +0100

    function that prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -48,11 +48,9 @@ void print_all(const char * const format, ...)
 			{
 				printf("(nil)");
 				break;
-			}
-			printf("%s", str);
+			} printf("%s", str);
 			break;
 		} i++;
 	}
-	printf("\n");
-	va_end(valist);
+	printf("\n"), va_end(valist);
 }
